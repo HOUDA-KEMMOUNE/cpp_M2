7722ac079560a3d5f22e66a7c6ae848091018884
HOUDA-KEMMOUNE <houdakemmoune2006@gmail.com>
2025-10-19 17:20:03 +0100

    ex00 --> 78%

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,40 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void)
+{
+	fixed_point = 0;
+	std::cout << "Default constructor called\n";
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called\n";
+}
+
+Fixed::Fixed(const Fixed &old)
+{
+	fixed_point = old.fixed_point;
+	std::cout << "Copy constructor called\n";
+}
+
+Fixed &Fixed::operator=(const Fixed &old)
+{
+	std::cout << "Copy assignment operator called\n";
+	if (this != &old)
+	{
+		this->fixed_point = old.getRawBits();
+	}
+	return (*this);
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called\n";
+	return (fixed_point);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	fixed_point = raw;
+	// std::cout << fixed_point << "\n";
+}
---
+++ ex00/Fixed.hpp
@@ -0,0 +1,24 @@
+# ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+#include <string>
+
+class   Fixed
+{
+	private:
+		int	fixed_point;
+		static const int	fractional_bits = 8;
+
+	public:
+		//Roba3i l9ahar
+		Fixed(void);
+		Fixed(const Fixed &old);
+		Fixed &operator=(const Fixed &old);
+		~Fixed(void);
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+};
+
+# endif
